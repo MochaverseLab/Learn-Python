1405b24dc3bfce557324ea8238bc46d66f2d9d32
Atyla A. Al Harits <damsadam78@gmail.com>
2023-10-09 22:27:11 +0700

    add more foders

---
+++ 000_Python-Dasar/Message.py
@@ -0,0 +1,9 @@
+# Halo temen-temen semua selamat datang di folder Python Dasar
+# mulai dari folder ini sampai seterusnya kita akan mempelajari fundamental dari Bahasa Python
+# mohon maaf jika penjelasan yang saya sampaikan sulit dimengerti atau kurang lengkap
+# silahakan ikuti pembelajaran ini sesuai no urut yang saya susun agar tidak ada informasi yang tertinggal
+
+# akan saya usahakan update sesering mungkin
+# hal-hal yang saya pelajari akan saya catat di repository ini
+
+# Terimakasih :)
---
+++ 004_tipe-data/tipe-data.py
@@ -0,0 +1,30 @@
+# Python memiliki banyak jenis tipe data
+# diantaranya adalah...
+
+"""
+- string     => tipe data yang menyatakan/mewakili huruf, angka, simbol, dan lain-lain
+- integer    => tipe data yang menyatakan/mewakili angka bilangan bulat
+- float      => tipe data yang menyatakan/mewakili angka bilangan pecahan
+- boolean    => tipe data yang menyatakan/mewakili benar (True) dan salah (False)
+- list/array => tipe data yang dapat menampung banyak data sekaligus
+"""
+
+#** <=================================================================> **#
+#** <=================================================================> **#
+
+
+# string
+nama = "Al Harits"
+
+# integer
+umur = 17
+
+# float
+pi = 3.14
+
+# boolean
+maried = False
+active = True
+
+# list/array
+address = ["Tasikmalaya", 123, True, ["Hello", "World"]]
